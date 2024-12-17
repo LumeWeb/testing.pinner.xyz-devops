7cf5d084a3a430f5d70c1bd6fdf76ff246efd90f
Derrick Hammer <derrick@derrickhammer.com>
2024-12-16 22:21:16 -0500

    fix: update domain records because cloudns does not want the zone in the name

--- layers/portal/dns.tf
+++ layers/portal/dns.tf
@@ -1,5 +1,10 @@
+locals {
+  renterd_cluster_bus_dns_name = replace(module.renterd_cluster.bus.dns_fqdn, format(".%s", local.base_domain), "")
+  renterd_cluster_autopilot_dns_name = replace(module.renterd_cluster.autopilot.dns_fqdn, format(".%s", local.base_domain), "")
+}
+
 resource "cloudns_dns_record" "renterd_bus" {
-  name  =module.renterd_cluster.bus.dns_fqdn
+  name  = local.renterd_cluster_bus_dns_name
   zone  = var.domain_zone
   type  = "CNAME"
   value = module.renterd_cluster.bus.host
@@ -7,7 +12,7 @@ resource "cloudns_dns_record" "renterd_bus" {
 }
 
 resource "cloudns_dns_record" "renterd_autopilot" {
-  name  = module.renterd_cluster.autopilot.dns_fqdn
+  name  =  local.renterd_cluster_autopilot_dns_name
   zone  = var.domain_zone
   type  = "CNAME"
   value = module.renterd_cluster.autopilot.host
@@ -16,7 +21,7 @@ resource "cloudns_dns_record" "renterd_autopilot" {
 
 resource "cloudns_dns_record" "renterd_workers" {
   count = length(module.renterd_cluster.workers)
-  name  = module.renterd_cluster.workers[count.index].dns_fqdn
+  name = replace(module.renterd_cluster.workers[count.index].dns_fqdn, format(".%s", local.base_domain), "")
   zone  = var.domain_zone
   type  = "CNAME"
   value = module.renterd_cluster.workers[count.index].host
