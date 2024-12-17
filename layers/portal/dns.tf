616dc60e57793482956d5746bd832fde04c7cbf7
Derrick Hammer <derrick@derrickhammer.com>
2024-12-16 22:41:28 -0500

    fix: use domain_zone not base_domain

--- layers/portal/dns.tf
+++ layers/portal/dns.tf
@@ -1,6 +1,6 @@
 locals {
-  renterd_cluster_bus_dns_name = replace(module.renterd_cluster.bus.dns_fqdn, format(".%s", local.base_domain), "")
-  renterd_cluster_autopilot_dns_name = replace(module.renterd_cluster.autopilot.dns_fqdn, format(".%s", local.base_domain), "")
+  renterd_cluster_bus_dns_name = replace(module.renterd_cluster.bus.dns_fqdn, format(".%s", var.domain_zone), "")
+  renterd_cluster_autopilot_dns_name = replace(module.renterd_cluster.autopilot.dns_fqdn, format(".%s", var.domain_zone), "")
 }
 
 resource "cloudns_dns_record" "renterd_bus" {
@@ -21,7 +21,7 @@ resource "cloudns_dns_record" "renterd_autopilot" {
 
 resource "cloudns_dns_record" "renterd_workers" {
   count = length(module.renterd_cluster.workers)
-  name = replace(module.renterd_cluster.workers[count.index].dns_fqdn, format(".%s", local.base_domain), "")
+  name = replace(module.renterd_cluster.workers[count.index].dns_fqdn, format(".%s", var.domain_zone), "")
   zone  = var.domain_zone
   type  = "CNAME"
   value = module.renterd_cluster.workers[count.index].host
