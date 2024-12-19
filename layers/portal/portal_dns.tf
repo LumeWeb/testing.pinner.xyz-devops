562bd5e7ffd77702ab3f76d7f38cde51f9d8dfd9
Derrick Hammer <derrick@derrickhammer.com>
2024-12-19 02:05:27 -0500

    fix: dns files were switched

--- layers/portal/portal_dns.tf
+++ layers/portal/portal_dns.tf
@@ -1,29 +1,7 @@
-locals {
-  renterd_cluster_bus_dns_name = replace(module.renterd_cluster.bus.dns_fqdn, format(".%s", var.domain_zone), "")
-  renterd_cluster_autopilot_dns_name = replace(module.renterd_cluster.autopilot.dns_fqdn, format(".%s", var.domain_zone), "")
-}
-
-resource "cloudns_dns_record" "renterd_bus" {
-  name  = local.renterd_cluster_bus_dns_name
+resource "cloudns_dns_record" "portal" {
+  name  = "@"
   zone  = var.domain_zone
-  type  = "CNAME"
-  value = module.renterd_cluster.bus.host
-  ttl   = "600"
-}
-
-resource "cloudns_dns_record" "renterd_autopilot" {
-  name  =  local.renterd_cluster_autopilot_dns_name
-  zone  = var.domain_zone
-  type  = "CNAME"
-  value = module.renterd_cluster.autopilot.host
-  ttl   = "600"
-}
-
-resource "cloudns_dns_record" "renterd_workers" {
-  count = length(module.renterd_cluster.workers)
-  name = replace(module.renterd_cluster.workers[count.index].dns_fqdn, format(".%s", var.domain_zone), "")
-  zone  = var.domain_zone
-  type  = "CNAME"
-  value = module.renterd_cluster.workers[count.index].host
+  type  = "A"
+  value = module.portal.ip_address
   ttl   = "600"
 }
