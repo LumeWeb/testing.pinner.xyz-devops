16002f1f8ff7cfe0ee305108b381d5035fa28b0f
Derrick Hammer <derrick@derrickhammer.com>
2024-12-18 22:20:51 -0500

    refactor: split up main.tf

--- layers/portal/renterd.tf
+++ layers/portal/renterd.tf
@@ -1,37 +1,3 @@
-# MySQL
-
-module "mysql" {
-  source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/db/mysql?ref=develop"
-
-  name              = "mysql"
-
-  root_password     = var.mysql_root_password
-  environment  = local.environment
-  backups_enabled   =  false
-
-  resources = {
-    cpu = {
-      cores = 2
-    }
-    memory = {
-      size = 4
-      unit = "Gi"
-    }
-    storage = {
-      size = 10
-      unit = "Gi"
-    }
-    persistent_storage = {
-      size  = 20
-      unit  = "Gi"
-      class = "beta3"
-    }
-  }
-
-  allowed_providers = var.allowed_providers
-  placement_attributes = local.placement_attributes
-}
-
 # Renterd Cluster
 module "renterd_cluster" {
   source = "git::https://github.com/LumeWeb/terraform-modules.git//modules/compute/renterd-cluster?ref=develop"
