c08c519819bc147c06b5711908a24f9825f63411
Derrick Hammer <derrick@derrickhammer.com>
2024-12-19 02:49:55 -0500

    refactor: create local.core_state

--- layers/portal/locals.tf
+++ layers/portal/locals.tf
@@ -1,9 +1,10 @@
 locals {
-  base_domain = data.terraform_remote_state.remote_states.outputs.base_domain
-  environment = data.terraform_remote_state.remote_states.outputs.environment
+  core_state = data.terraform_remote_state.remote_states.outputs
+  base_domain = local.core_state.base_domain
+  environment = local.core_state.environment
   renterd_base_domain = format("node.%s", local.base_domain)
   placement_attributes = {
     "lumeweb" = true
   }
-  s3_endpoint = data.terraform_remote_state.remote_states.outputs.renterd_s3_endpoint
+  s3_endpoint = local.core_state.renterd_s3_endpoint
 }
