e0c6610105490124bcfa22a3c54065fb362df270
Derrick Hammer <derrick@derrickhammer.com>
2024-12-21 20:34:35 -0500

    refactor: add portal_wildcard dns records

--- layers/portal/portal_dns.tf
+++ layers/portal/portal_dns.tf
@@ -4,8 +4,17 @@ locals {
 
 resource "cloudns_dns_record" "portal" {
   count = length(module.portal)
-  name  =  local.portal_dns_name
-  zone  =  var.domain_zone
+  name  = local.portal_dns_name
+  zone  = var.domain_zone
+  type  = "A"
+  value = module.portal[count.index].ip_address
+  ttl   = "600"
+}
+
+resource "cloudns_dns_record" "portal_wildcard" {
+  count = length(module.portal)
+  name = format("*.%s", local.portal_dns_name)
+  zone  = var.domain_zone
   type  = "A"
   value = module.portal[count.index].ip_address
   ttl   = "600"
